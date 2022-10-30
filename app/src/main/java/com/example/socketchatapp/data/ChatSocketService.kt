2dc7be09b76d5b78b72d78b75e994f5f49826f8d
CherifiMi <cherifi154949@gmail.com>
2022-10-30 17:38:37 +0100

    ChatSocketService

--- app/src/main/java/com/example/socketchatapp/data/ChatSocketService.kt
+++ app/src/main/java/com/example/socketchatapp/data/ChatSocketService.kt
@@ -1,4 +1,25 @@
 package com.example.socketchatapp.data
 
+import com.example.socketchatapp.domain.model.Message
+import com.example.socketchatapp.util.Resource
+import kotlinx.coroutines.flow.Flow
+
 interface ChatSocketService {
+    suspend fun initSession(
+        username: String
+    ): Resource<Unit>
+
+    suspend fun sendMessage(message: String)
+
+    fun observeMessage(): Flow<Message>
+
+    suspend fun closeSession()
+
+    companion object{
+        const val BASE_URL = "ws://ktor-chat-server-mito.herokuapp.com"
+    }
+
+    sealed class Endpoints(val url: String){
+        object ChatSocket: Endpoints("$BASE_URL/chat-socket")
+    }
 }
