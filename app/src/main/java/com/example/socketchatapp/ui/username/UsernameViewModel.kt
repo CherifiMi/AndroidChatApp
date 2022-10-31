c98cbb929a5c03cd23156d3bd4fbc9193c5e6330
CherifiMi <cherifi154949@gmail.com>
2022-10-31 03:25:41 +0100

    UsernameViewModel init

---
+++ app/src/main/java/com/example/socketchatapp/ui/username/UsernameViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.socketchatapp.ui.username
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class UsernameViewModel @Inject constructor() : ViewModel() {
+
+}
