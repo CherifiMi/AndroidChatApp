021ed815145aa814954d10698c11672e1d6e288c
CherifiMi <cherifi154949@gmail.com>
2022-10-31 03:25:07 +0100

    ChatViewModel

--- app/src/main/java/com/example/socketchatapp/ui/chat/ChatViewModel.kt
+++ app/src/main/java/com/example/socketchatapp/ui/chat/ChatViewModel.kt
@@ -4,9 +4,16 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.socketchatapp.data.ChatSocketService
 import com.example.socketchatapp.data.MessageService
+import com.example.socketchatapp.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,5 +28,68 @@ class ChatViewModel @Inject constructor(
 
     private val _messageText = mutableStateOf("")
     val messageText: State<String> = _messageText
+
+    private val _toastEvent = MutableSharedFlow<String>()
+    val toastEvent = _toastEvent.asSharedFlow()
+
+
+    fun connectToChat(){
+        getAllMessages()
+        savedStateHandle.get<String>("username")?.let { username->
+            viewModelScope.launch {
+                val result = chatSocketService.initSession(username)
+                when(result){
+                    is Resource.Success -> {
+                        chatSocketService.observeMessage()
+                            .onEach { message ->
+                                val newList = state.value.messages.toMutableList().apply {
+                                    add(0, message)
+                                }
+                                _state.value = state.value.copy(
+                                    messages = newList
+                                )
+                            }.launchIn(viewModelScope)
+                    }
+                    is Resource.Error -> {
+                        _toastEvent.emit(result.message ?: "IDK ERR")
+                    }
+                }
+            }
+        }
+    }
+
+    fun onMessageChange(message: String){
+        _messageText.value = message
+    }
+
+    fun disconnect(){
+        viewModelScope.launch {
+            chatSocketService.closeSession()
+        }
+    }
+
+    fun getAllMessages(){
+        viewModelScope.launch {
+            _state.value = state.value.copy(isLoading = true)
+            val result = messageService.getAllMessages()
+            _state.value = state.value.copy(
+                messages =  result,
+                isLoading = false
+            )
+        }
+    }
+
+    fun sendMessage(){
+        viewModelScope.launch {
+            if (messageText.value.isNotBlank()){
+                chatSocketService.sendMessage(messageText.value)
+            }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disconnect()
+    }
 
 }
